d2ce9b061fc712d247e3fc9685820f96194ba301
JASIL AHAMMED <jasilahammed7@gmail.com>
2025-11-30 16:42:15 +0530

    Update asort.php

--- PHP/prg3.php/asort.php
+++ PHP/prg3.php/asort.php
@@ -5,14 +5,14 @@
 <?php
 $name = array("gopi", "arjun", "afsa", "jasil");
 
-// Ascending order
+
 asort($name);
 echo "<h3>Ascending order (asort):</h3>";
 foreach($name as $n){
     echo $n . "<br>";
 }
 
-// Descending order
+
 arsort($name);
 echo "<h3>Descending order (arsort):</h3>";
 foreach($name as $n){
